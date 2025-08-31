fd9d04d18151ffff7cbd5a0163e6df49cff491a8
Tanishaaa29 <singhtanisha2911@gmail.com>
2025-08-31 22:55:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// Simple contact form alert
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you! Your message has been sent.');
+  this.reset();
+});
